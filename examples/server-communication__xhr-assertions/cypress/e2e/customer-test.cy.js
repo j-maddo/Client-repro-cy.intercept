a5a877d75ba874e9a49190c8d7b678feae554c80
Jireh <jireh@cypress.io>
2022-09-22 14:09:17 -0400

    update the intercept url

--- examples/server-communication__xhr-assertions/cypress/e2e/customer-test.cy.js
+++ examples/server-communication__xhr-assertions/cypress/e2e/customer-test.cy.js
@@ -2,9 +2,10 @@ describe('Debug', () => {
   it('Can\'t intercept every algolia call', () => {
     cy.viewport('macbook-11')
     cy.setCookie('gdid', 'cypress')
-    cy.intercept('**indexes/*/queries**').as('algolia')
+    cy.intercept('**/indexes/*/queries').as('algolia')
+    // cy.request("POST","https://e7phe9bb38-dsn.algolia.net/1/indexes/*/queries");
+    cy.visit('https://www.gofundme.com/s?q=help')
 
-    cy.visit('http://www.gofundme.com/s?q=help')
     cy.wait('@algolia').its('response.statusCode').should('eq', 200)
   })
 })
